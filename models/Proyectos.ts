606e4d4807b61845340b8426daf34b628b471aa3
Andres Campuzano <acampuz1@gmail.com>
2021-11-11 10:25:18 -0500

    Creado modelo de inscripcion

--- models/Proyectos.ts
+++ models/Proyectos.ts
@@ -1,54 +1,55 @@
-import {Schema, model} from 'mongoose';
-import {Enum_EstadoProyecto, Enum_FaseProyecto} from "./Enums";
+import { Schema, model } from 'mongoose';
+import { Enum_EstadoProyecto, Enum_FaseProyecto } from "./Enums";
+
 import { UserModel } from './Usuarios';
 
 interface Project {
-    nombre: string,
-    // objetivos: Objetivo[],
-    presupuesto: number,
-    fechaInicio: Date,
-    fechaFin: Date,
-    lider: Schema.Types.ObjectId,
-    estadoProyecto: Enum_EstadoProyecto,
-    faseProyecto: Enum_FaseProyecto,
-    // inscripciones: Inscripcion[],
-    // avances: Avance[],
+  nombre: string,
+  // objetivos: Objetivo[],
+  presupuesto: number,
+  fechaInicio: Date,
+  fechaFin: Date,
+  lider: Schema.Types.ObjectId,
+  estadoProyecto: Enum_EstadoProyecto,
+  faseProyecto: Enum_FaseProyecto,
+  // inscripciones: Inscripcion[],
+  // avances: Avance[],
 }
 
-const ProjectSchema = new Schema <Project>({
-    nombre: {
-        type: String,
-        required: true,
-      },
-      presupuesto: {
-        type: Number,
-        required: true,
-      },
-      fechaInicio: {
-        type: Date,
-        required: true,
-      },
-      fechaFin: {
-        type: Date,
-        required: true,
-      },
-      lider: {
-        type: Schema.Types.ObjectId,
-        ref: UserModel,
-      },
-      estadoProyecto: {
-        type: String,
-        enum: Enum_EstadoProyecto,
-        default: Enum_EstadoProyecto.inactivo,
-      },
-      faseProyecto: {
-        type: String,
-        enum: Enum_FaseProyecto,
-        default: Enum_FaseProyecto.nulo,
-      },
-      
+const ProjectSchema = new Schema<Project>({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  presupuesto: {
+    type: Number,
+    required: true,
+  },
+  fechaInicio: {
+    type: Date,
+    required: true,
+  },
+  fechaFin: {
+    type: Date,
+    required: true,
+  },
+  lider: {
+    type: Schema.Types.ObjectId,
+    ref: UserModel,
+  },
+  estadoProyecto: {
+    type: String,
+    enum: Enum_EstadoProyecto,
+    default: Enum_EstadoProyecto.inactivo,
+  },
+  faseProyecto: {
+    type: String,
+    enum: Enum_FaseProyecto,
+    default: Enum_FaseProyecto.nulo,
+  },
+
 });
 
-const ProjectModel = model('Project',  ProjectSchema);
+const ProjectModel = model('Project', ProjectSchema);
 
-export {ProjectModel};
+export { ProjectModel };
